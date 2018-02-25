331e0b6703a8663f4c19dc958fa5bbf7cdf2905f
aljazsim <aljazsim@gmail.com>
2018-02-24 20:03:13 -0500

    - added nuget publishing script

---
+++ Build/publishNuGetPackage.ps1
@@ -0,0 +1,11 @@
+$ErrorPreference = 'Stop'
+
+$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+$nuget = "C:\Program Files (x86)\NuGet\nuget.exe"
+
+$projectFilePath = "..\Source\DefensiveProgrammingFramework\DefensiveProgrammingFramework.csproj";
+$nugetPackageFilePath = "..\Source\DefensiveProgrammingFramework\bin\Release\DefensiveProgrammingFramework.1.0.1.nupkg"
+
+& "$msbuild" "$projectFilePath" /t:pack /p:Configuration=Release
+
+& "$nuget" push "$nugetPackageFilePath" xxx -Source https://api.nuget.org/v3/index.json
